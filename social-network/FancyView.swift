59521546dd9c57d7ac3e8b3692d88fa2c5803ff3
Alexandru Corut <r3dm4n@me.com>
2017-05-22 18:11:16 +0300

    social-feed-UI

--- social-network/FancyView.swift
+++ social-network/FancyView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DropShadow: UIView {
+class FancyView: UIView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,6 +17,7 @@ class DropShadow: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
         
     }
 }
