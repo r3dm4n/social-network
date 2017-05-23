46d6968ba932cdf56bb7e4e0057a19e4d721441f
Alexandru Corut <r3dm4n@me.com>
2017-05-23 10:37:28 +0300

    parse firebase data

--- social-network/FeedVC.swift
+++ social-network/FeedVC.swift
@@ -14,12 +14,28 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP", snap)
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        })
+        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -27,7 +43,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -45,4 +61,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 
 }
+
+
+
+
+
+
 
