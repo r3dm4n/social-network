d6313c0c57bb4417522cfabb53bccf5ba84d22a1
Alexandru Corut <r3dm4n@me.com>
2017-05-23 10:57:31 +0300

    display data in UI

--- social-network/FeedVC.swift
+++ social-network/FeedVC.swift
@@ -11,9 +11,9 @@ import SwiftKeychainWrapper
 import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
     @IBOutlet weak var tableView: UITableView!
-    
+
     var posts = [Post]()
 
     override func viewDidLoad() {
@@ -21,7 +21,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
                 for snap in snapshot {
@@ -35,32 +35,41 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             }
             self.tableView.reloadData()
         })
-        
+
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+
+        let post = posts[indexPath.row]
+
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+
+        }
     }
 
-    
 
-    @IBAction func signOutTapped(_ sender: Any) {
-        let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
-        print(keychainResult)
-        try! FIRAuth.auth()?.signOut()
-        performSegue(withIdentifier: "goToSignIn", sender: nil)
+
+        @IBAction func signOutTapped(_ sender: Any) {
+            let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
+            print(keychainResult)
+            try! FIRAuth.auth()?.signOut()
+            performSegue(withIdentifier: "goToSignIn", sender: nil)
+        }
+
+
     }
-
-
-}
 
 
 
